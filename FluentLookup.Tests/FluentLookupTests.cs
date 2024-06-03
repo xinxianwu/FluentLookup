bc809ffbc98d5bc15d77e4df781c126190216c8b
Xinxian <sgmp2555029@gmail.com>
2024-06-04 00:37:50 +0800

    feat: implement Except()

---
+++ FluentLookup.Tests/FluentLookupTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using FluentLookup.Extensions;
+using NUnit.Framework;
+
+namespace FluentLookup.Tests;
+
+[TestFixture]
+public class FluentLookupTests
+{
+    [Test]
+    public void except_element()
+    {
+        var range = Enumerable.Range(0, 10);
+
+        var list = range.Except(x => x % 2 == 0)
+            .ToList();
+
+        list.Should().BeEquivalentTo(new List<int>()
+        {
+            1, 3, 5, 7, 9
+        });
+    }
+}
---
+++ FluentLookup/Extensions/LookupExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FluentLookup.Extensions
+{
+    public static class LookupExtensions
+    {
+        public static IEnumerable<TModel> Except<TModel>(this IEnumerable<TModel> range, Func<TModel, bool> func)
+        {
+            return range
+                .Where(x => !func.Invoke(x));
+        }
+    }
+}
