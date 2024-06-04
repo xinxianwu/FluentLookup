9acf89cadcb7bf26efc7d98abbc5aac15741b5ca
Xinxian <sgmp2555029@gmail.com>
2024-06-04 20:57:24 +0800

    feat: implement Difference()

--- FluentLookup.Tests/FluentLookupTests.cs
+++ FluentLookup.Tests/FluentLookupTests.cs
@@ -1,5 +1,6 @@
 ﻿using FluentAssertions;
 using FluentLookup.Extensions;
+using FluentLookup.Tests.Models;
 using NUnit.Framework;
 
 namespace FluentLookup.Tests;
@@ -19,5 +20,29 @@ public class FluentLookupTests
         {
             1, 3, 5, 7, 9
         });
+    }
+
+    [Test]
+    public void exclude_item_based_on_key_selector()
+    {
+        var customers = new List<CustomerModel>()
+        {
+            new CustomerModel() { Year = 2020, Name = "John", Sex = "M" },
+            new CustomerModel() { Year = 2019, Name = "Tom", Sex = "M" },
+            new CustomerModel() { Year = 2017, Name = "Tina", Sex = "F" },
+        };
+
+        var excludedSexes = new HashSet<string>() { "F" };
+
+        var filteredCustomers = customers
+            .Difference(excludedSexes, x => x.Sex)
+            .ToList();
+
+        filteredCustomers.Should().BeEquivalentTo(new List<CustomerModel>()
+            {
+                new CustomerModel() { Year = 2020, Name = "John", Sex = "M" },
+                new CustomerModel() { Year = 2019, Name = "Tom", Sex = "M" },
+            }
+        );
     }
 }
