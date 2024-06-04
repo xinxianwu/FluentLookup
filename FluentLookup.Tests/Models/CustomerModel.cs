de5615441fee85dada477bd3b17d20a70d9d9481
Xinxian <xinxian.wu@onelab.tw>
2024-06-04 20:57:24 +0800

    feat: implement Difference()

---
+++ FluentLookup.Tests/Models/CustomerModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FluentLookup.Tests.Models;
+
+public class CustomerModel
+{
+    public int Year { get; set; }
+    public string Name { get; set; }
+    public string Sex { get; set; }
+}
--- FluentLookup/Extensions/LookupExtensions.cs
+++ FluentLookup/Extensions/LookupExtensions.cs
@@ -6,10 +6,25 @@ namespace FluentLookup.Extensions
 {
     public static class LookupExtensions
     {
-        public static IEnumerable<TModel> Except<TModel>(this IEnumerable<TModel> range, Func<TModel, bool> func)
+        public static IEnumerable<TModel> Except<TModel>(this IEnumerable<TModel> source, Func<TModel, bool> func)
         {
-            return range
+            return source
                 .Where(x => !func.Invoke(x));
+        }
+
+        /// <summary>
+        /// exclude items based on the key selector
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="exclusionSet"></param>
+        /// <param name="selector"></param>
+        /// <typeparam name="TModel"></typeparam>
+        /// <typeparam name="TAnotherModel"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<TModel> Difference<TModel, TAnotherModel>(this IEnumerable<TModel> source, ISet<TAnotherModel> exclusionSet, Func<TModel, TAnotherModel> selector)
+        {
+            return source
+                .Where(x => !exclusionSet.Contains(selector(x)));
         }
     }
 }
